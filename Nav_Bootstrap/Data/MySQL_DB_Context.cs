449671f63aa84e7b741ce62f0ee54afb30570692
Kőrösi Zoltán <prokore88@gmail.com>
2024-04-04 18:23:04 +0200

    Add project files.

---
+++ Nav_Bootstrap/Data/MySQL_DB_Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nav_Bootstrap.Modell;
+
+namespace Nav_Bootstrap.Data
+{
+    public class MySQL_DB_Context : DbContext
+    {
+        public MySQL_DB_Context(DbContextOptions<MySQL_DB_Context> options) : base(options){}
+        public DbSet<PaciHotel> PaciHotel { get; set; } = default;
+    }
+     
+}
---
+++ Nav_Bootstrap/Model/PaciHotel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Nav_Bootstrap.Modell
+{
+    public class PaciHotel
+
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required(ErrorMessage ="A név mező kitöltése kötelező!")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="A helyszín mező kitöltése kötelező!")]
+        public string Location { get; set; }
+        [Required(ErrorMessage ="Az ár mező kitöltése kötelező!")]
+        public int Price { get; set; }
+        public double Rating { get; set; }
+        public string LovakSzama { get; set; } = "Nincs információ!";
+        [Required(ErrorMessage ="A fizetési lehetőség kiválasztása kötelező!")]
+        public bool CardorNot { get; set; } = false;
+
+    }
+}
